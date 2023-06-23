17e358fbfa3397055fa43b9f7e5ba89b2520a8c9
Shaun2003 <tshimangadzoshaun21@gmail.com>
2023-06-23 00:58:17 -0700

    monty all the way

---
+++ nothing.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_nop- nothing
+ * @head: stack
+ * @counter: line_num
+ * Return:
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ rotate_b.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * f_rotr - rotate stack to the bottom
+ * @head: stack
+ * @counter: line_num
+ * Return:
+ */
+
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+	stack_t *copy;
+
+	copy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
+}
